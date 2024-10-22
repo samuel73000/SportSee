1200eed69f92872648c31d9962cd82fb39df4f36
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-10-22 10:34:07 +0200

    SportSee

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Home from "./Page/Home/Home.jsx"
+import Header from "./composant/Header/Header.jsx"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Header />
+    <Home />
   </StrictMode>,
 )
