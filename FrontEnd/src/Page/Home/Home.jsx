d4c108114d3501cd7c52bb6ee4e024c68aa7884d
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-10-23 06:23:33 +0200

    SportSee

--- FrontEnd/src/Page/Home/Home.jsx
+++ FrontEnd/src/Page/Home/Home.jsx
@@ -1,7 +1,40 @@
-import "./Home.css"
+import { useState, useEffect } from "react";
+import "./Home.css";
+
 export default function Home() {
+  const [name, setName] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/user/18")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur réseau lors du chargement des données");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setName(data.data.userInfos.firstName);
+      })
+      .catch((error) => {
+        console.error("Erreur:", error);
+        setError(error.message); 
+      });
+  }, []); 
+  if (error) {
+    return <div>Erreur : {error}</div>;
+  }
+
   return (
-    <div>
-    </div>
+    <section>
+      <div className="container-titre-home">
+        <h1 className="titre-home">
+          Bonjour <span className="spam-home">{name}</span>
+        </h1>
+        <p className="texte-titre-home">
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </div>
+    </section>
   );
 }
--- FrontEnd/src/composant/Header/Header.jsx
+++ FrontEnd/src/composant/Header/Header.jsx
@@ -1,8 +1,20 @@
-import "./Header.css"
+import "./Header.css";
+import logo from "../../assets/logo.png"
+import nav from '../../assets/nav.png'
 export default function Header() {
   return (
-  <section>
-    
-  </section>
-  )
+    <header>
+      <div className="header-top">
+      <img src={logo} alt="logo du site"  className="logo-header"/>
+      <a href="#" className="lien-header">Accueil</a>
+      <a href="#" className="lien-header">Profil</a>
+      <a href="#" className="lien-header">Réglage</a>
+      <a href="#" className="lien-header">Communauté</a>
+      </div>
+      <div className="header-left">
+        <img src={nav} alt="nav" className="logo-header-left" />
+        <p className="p-header-left">Copiryght, SportSee 2020</p>
+      </div>
+    </header>
+  );
 }
