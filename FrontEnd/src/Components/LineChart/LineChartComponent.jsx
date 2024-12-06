63bc01fabffd263f2c3410162ca6f14527265fb5
Samuel Pouard <thosam73000@gmail.com>
2024-12-06 23:53:09 +0100

    SportSee

--- FrontEnd/src/Components/LineChart/LineChartComponent.jsx
+++ FrontEnd/src/Components/LineChart/LineChartComponent.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import "./LineChartComponent.css";
 
 export default function LineChartPerf(props) {
@@ -16,23 +23,26 @@ export default function LineChartPerf(props) {
   // Fonction pour le tooltip personnalisé
   const CustomTooltip = ({ active, payload, coordinate }) => {
     if (active && payload && payload.length && coordinate) {
-      const { x } = coordinate; // Position x 
+      const { x } = coordinate; // Position x
       const tooltipText = `${payload[0].value} min`;
 
       // Calculer la largeur en fonction de la position de la souris sur le graphique
       const percentage = Math.min(Math.max((x / chartWidth) * 20, 0), 100);
 
-      let tooltipPosition = { left: `${x}px`, top: `${y}px`, transform: "translateX(-100%)" };
+      let tooltipPosition = {
+        left: `${x}px`,
+        top: `${y}px`,
+        transform: "translateX(-100%)",
+      };
 
       return (
         <div
-          className="custom-tooltip-lineChart"
+          className='custom-tooltip-lineChart'
           style={{
             ...tooltipPosition,
             width: `${percentage}vw`, // Largeur dynamique en fonction du graphique
-          }}
-        >
-          <p className="text-ToolTip">{tooltipText}</p>
+          }}>
+          <p className='text-ToolTip'>{tooltipText}</p>
         </div>
       );
     }
@@ -46,22 +56,21 @@ export default function LineChartPerf(props) {
   }));
 
   return (
-    <div className="line-chart-container">
-      <p className="titre-LineChart">Durée moyenne des sessions</p>
-      <div className="container-graphique-line-chart" ref={chartRef}>
-        <ResponsiveContainer width="100%" height="100%">
+    <div className='line-chart-container'>
+      <p className='titre-LineChart'>Durée moyenne des sessions</p>
+      <div className='container-graphique-line-chart' ref={chartRef}>
+        <ResponsiveContainer width='100%' height='100%'>
           <LineChart
             data={data}
-            margin={{ top: 5, right: 0, left: 0, bottom: 10 }}
-          >
+            margin={{ top: 5, right: 0, left: 0, bottom: 10 }}>
             <XAxis
-              dataKey="day"
+              dataKey='day'
               padding={{ left: 10, right: 10 }}
               tickLine={false}
               axisLine={false}
               tick={{ fill: "#FFFFFF", fontSize: 12, dx: -10 }}
               minTickGap={0}
-              interval="preserveStartEnd"
+              interval='preserveStartEnd'
               tickMargin={10}
               fillOpacity={0.5}
               style={{ transform: "scale(0.9)", transformOrigin: "bottom" }}
@@ -69,11 +78,11 @@ export default function LineChartPerf(props) {
             <YAxis domain={[0, "dataMax + 20"]} hide={true} />
             <Tooltip content={<CustomTooltip chartWidth={chartWidth} />} />
             <Line
-              type="monotone"
-              dataKey="sessionLength"
+              type='monotone'
+              dataKey='sessionLength'
               activeDot={{ r: 8 }}
               dot={false}
-              stroke="#FFFFFF"
+              stroke='#FFFFFF'
             />
           </LineChart>
         </ResponsiveContainer>
@@ -81,11 +90,3 @@ export default function LineChartPerf(props) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
